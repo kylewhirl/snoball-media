92c147058f688197037b0e0cf05871c12cc3f75b
kylewhirl <kyleworrall54@gmail.com>
2025-04-24 22:22:24 -0700

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,15 @@ import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = {
   title: "Snoball Media",
   description: "Graphic and Web Design Company",
-    generator: 'v0.dev'
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" }
+    ],
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico"
+  },
 }
 
 export default function RootLayout({
